92bbf061aad95f79c89bb85a4ce9659d0268fc8a
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2022-09-15 01:09:22 -0300

    story map

--- src/Stories.js
+++ src/Stories.js
@@ -2,20 +2,19 @@ import Story from "./Story"
 
 
 export default function Stories(){
+    const perfils = [
+        {fotoStory:'assets/img/9gag.svg', nomeStory:'9gag'},
+        {fotoStory:'assets/img/meowed.svg',nomeStory:'meowed'},
+        {fotoStory:'assets/img/barked.svg',nomeStory:'barked'},
+        {fotoStory:'assets/img/nathanwpylestrangeplanet.svg',nomeStory:'nathanwpylestrangeplanet'},
+        {fotoStory:'assets/img/wawawicomics.svg',nomeStory:'wawawicomics'},
+        {fotoStory:'assets/img/respondeai.svg',nomeStory:'respondeai'},
+        {fotoStory:'assets/img/filomoderna.svg',nomeStory:'filomoderna'},
+        {fotoStory:'assets/img/memeriagourmet.svg',nomeStory:'memeriagourmet'},
+    ]
     return (
         <div class = "stories">
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
-        <Story></Story>
+            {perfils.map((obj) => <Story imagem = {obj.fotoStory} usuario = {obj.nomeStory}></Story>)}
         <div class="setinha">
             <ion-icon name="chevron-forward-circle" role="img" class="md hydrated" aria-label="círculo para frente chevron"></ion-icon>
         </div>
